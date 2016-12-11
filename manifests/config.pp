a15f828fc08d4e2351451d037ccdb0542977b7dc
Tim Sullivan <tsullivan@timatooth.com>
2016-12-11 16:53:27 +1300

    use file_line all the things

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,18 +6,22 @@ class s_minecraft::config ($server_properties = '/opt/minecraft/server.propertie
     line  => 'online-mode=false',
   }
   file_line { 'max-players':
+    match => '^max-players='
     path => $server_properties,
     line => 'max-players=15',
   }
   file_line { 'server-motd':
+    match => '^motd='
     path => $server_properties,
     line => 'motd=A minecraft server by Timatooth',
   }
   file_line { 'server-name':
+    match => '^server-name='
     path => $server_properties,
     line => 'server-name=WorldCraft Reborn',
   }
   file_line { 'view-distance':
+    match => '^view-distance='
     path => $server_properties,
     line => 'view-distance=20',
   }
