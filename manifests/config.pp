b855aecc63e7708db2d373ea95cf39c505d9857b
Tim Sullivan <tsullivan@timatooth.com>
2016-12-11 16:57:09 +1300

    Commas help eh...

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,27 +2,27 @@ class s_minecraft::config ($server_properties = '/opt/minecraft/server.propertie
 
   file_line { 'online-mode':
     path  => $server_properties,
-    match => '^online-mode='
+    match => '^online-mode=',
     line  => 'online-mode=false',
   }
   file_line { 'max-players':
-    match => '^max-players='
-    path => $server_properties,
-    line => 'max-players=15',
+    match => '^max-players=',
+    path  => $server_properties,
+    line  => 'max-players=15',
   }
   file_line { 'server-motd':
-    match => '^motd='
-    path => $server_properties,
-    line => 'motd=A minecraft server by Timatooth',
+    match => '^motd=',
+    path  => $server_properties,
+    line  => 'motd=A minecraft server by Timatooth',
   }
   file_line { 'server-name':
-    match => '^server-name='
-    path => $server_properties,
-    line => 'server-name=WorldCraft Reborn',
+    match => '^server-name=',
+    path  => $server_properties,
+    line  => 'server-name=WorldCraft Reborn',
   }
   file_line { 'view-distance':
-    match => '^view-distance='
-    path => $server_properties,
-    line => 'view-distance=20',
+    match => '^view-distance=',
+    path  => $server_properties,
+    line  => 'view-distance=20',
   }
 }
